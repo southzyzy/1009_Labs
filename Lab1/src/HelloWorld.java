d4e2fdd307a906d164cc661bc8636a05363919f2
southzyzy <tanzhaoyea98.tzy@gmail.com>
2021-01-13 13:04:15 +0800

    Upload Lab1

---
+++ Lab1/src/HelloWorld.java
@@ -0,0 +1,90 @@
+// Lab 1 Exercises
+// Author @ Tan Zhao Yea (1802992)
+
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+//        Task1 t1 = new Task1();
+//        System.out.println(t1.getAnimal());
+
+        Task2 t2 = new Task2();
+        System.out.printf("The BMI is %.2f\n", t2.getBMI());
+        System.out.println(t2.getBmiStatus());
+    }
+}
+
+// Source Code for Task 1
+class Task1 {
+    String animal = "";
+    public Task1() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+
+        int year = in.nextInt();
+
+        switch (year % 12) {
+            case 0 -> this.animal = "monkey";
+            case 1 -> this.animal = "rooster";
+            case 2 -> this.animal = "dog";
+            case 3 -> this.animal = "pig";
+            case 4 -> this.animal = "rat";
+            case 5 -> this.animal = "ox";
+            case 6 -> this.animal = "tiger";
+            case 7 -> this.animal = "rabbit";
+            case 8 -> this.animal = "dragon";
+            case 9 -> this.animal = "snake";
+            case 10 -> this.animal = "horse";
+            case 11 -> this.animal = "sheep";
+        }
+    }
+    public String getAnimal() {
+        return animal;
+    }
+}
+
+// Source code for Task 2
+class Task2 {
+    // BMI Formula: [weight (lb) / height (in) / height (in)] x 703
+    String bmiStatus = "";
+    double bmi;
+
+    public Task2() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter weight in pounds: ");
+        double weight = in.nextDouble();
+
+        // Reset the buffer
+        in.nextLine();
+
+        System.out.print("Enter height in inches: ");
+        double height = in.nextDouble();
+        // Calculate the BMI
+        this.bmi = (weight / height / height) * 703;
+
+        // Check if BMI is Underweight
+        if (bmi < 18.5) {
+            this.bmiStatus = "Underweight";
+        }
+        // Check if BMI is Normal
+        else if (18.5 <= bmi && bmi < 25.0) {
+            this.bmiStatus = "Normal";
+        }
+        // Check if BMI is Overweight
+        else if (25.0 <= bmi && bmi < 30.0) {
+            this.bmiStatus = "Overweight";
+        }
+        // Check if BMI is Obese
+        else if (30.0 <= bmi) {
+            this.bmiStatus = "Obese";
+        }
+    }
+    public double getBMI() {
+        return bmi;
+    }
+    public String getBmiStatus(){
+        return bmiStatus;
+    }
+}
+
